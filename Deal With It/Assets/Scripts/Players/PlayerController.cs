6aefb1e0f2c1798806bcc3de79cd6492ff5fe6f4
ElijahAlcibar <elijah.alcibar@obf.ateneo.edu>
2022-09-22 18:25:44 +0800

    Add play card feature

--- Deal With It/Assets/Scripts/Players/PlayerController.cs
+++ Deal With It/Assets/Scripts/Players/PlayerController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class PlayerController : MonoBehaviour
 
     // Array of cards in a player's hand (public if we're adding feature when other players can look into ur hand)
     public Action[] CardsInHand = new Action[5];
+    public Text[] CardsInHandText = new Text[5];
 
     // ActionCardDeck
     private ActionCardDeck ActionCardDeck;
@@ -41,11 +43,20 @@ public class PlayerController : MonoBehaviour
             // Makes sure that players have 5 cards every round
             // While there is an empty slot in the player's hand
             while(IndexOfEmptyElement != -1){
-                CardsInHand[IndexOfEmptyElement] = ActionCardDeck.GetRandomCard();
+                Action PickedCard = ActionCardDeck.GetRandomCard();
+
+                // Puts card in player's hand
+                CardsInHand[IndexOfEmptyElement] = PickedCard;
+
+                // Writes down the card name for a button
+                CardsInHandText[IndexOfEmptyElement].text = PickedCard.CardName;
+
                 IndexOfEmptyElement = Array.IndexOf(CardsInHand, null);
             }
-
-            Debug.Log("Next Round");
         }
+    }
+
+    public void PlayCard(int CardIndex){
+        Debug.Log("Player " + PlayerNumber + " has played the card " + CardsInHand[CardIndex].CardName);
     }
 }
