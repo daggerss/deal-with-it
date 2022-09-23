36914dc41c4ba8d1adc48b17adb97a73a0ca95f4
Dagny Eran Yenko <dagnyeran@gmail.com>
2022-09-24 03:58:14 +0800

    Set UI on NPCDisplay.cs

--- Deal With It/Assets/Scripts/Cards/NPCs/NPCDisplay.cs
+++ Deal With It/Assets/Scripts/Cards/NPCs/NPCDisplay.cs
@@ -1,11 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NPCDisplay : MonoBehaviour
 {
     public NPC npc;
 
+    /* ---------------------------- Card Information ---------------------------- */
+    public Text nameText;
+    public Text descriptionText;
+
+    public Image illustrationImage;
+
+    public Text energyText;
+    public Text joyText;
+    public Text sadnessText;
+    public Text fearText;
+    public Text angerText;
+
+    /* --------------------------------- Methods -------------------------------- */
     // Start is called before the first frame update
     void Start()
     {
@@ -31,5 +45,17 @@ public class NPCDisplay : MonoBehaviour
             npc.FearLvl = Random.Range(9, 12);
             npc.AngerLvl = Random.Range(9, 12);
         }
+
+        // Setting information UI
+        nameText.text = npc.CardName;
+        descriptionText.text = npc.CardDescription;
+
+        illustrationImage.sprite = npc.Illustration;
+
+        energyText.text = npc.EnergyLvl.ToString();
+        joyText.text = npc.JoyLvl.ToString();
+        sadnessText.text = npc.SadnessLvl.ToString();
+        fearText.text = npc.FearLvl.ToString();
+        angerText.text = npc.AngerLvl.ToString();
     }
 }
