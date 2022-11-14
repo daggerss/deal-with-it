9758a07ddda59b4ac84efd0baed9f25c6e75cda9
Dru Villamor <81793039+joshuavillamor@users.noreply.github.com>
2022-11-14 16:44:17 +0800

    Added Gameplay04 Increase size on hover functionality

---
+++ Deal With It/Assets/Scripts/PlayerCardHover.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerCardHover : MonoBehaviour , IPointerEnterHandler, IPointerExitHandler
+{
+    Vector3 cardScale;
+    Vector2 cardPosition;
+    int cardIndexNumber;
+
+    void Start(){
+        cardScale = this.transform.localScale;
+        cardPosition = this.transform.position;
+        cardIndexNumber = this.transform.GetSiblingIndex();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData){
+        this.transform.localScale = new Vector3(1.5F, 1.5F, 1.5F);
+        this.transform.position = new Vector2(this.transform.position.x, 350F);
+        this.transform.SetSiblingIndex(5);
+
+    }
+
+    public void OnPointerExit(PointerEventData eventData){
+        this.transform.localScale = cardScale;
+        this.transform.position = cardPosition;
+        this.transform.SetSiblingIndex(cardIndexNumber);
+
+    }
+}
