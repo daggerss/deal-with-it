0f379ca28a90e3bf3829f2cdff1c1e6cf128d4ef
Dagny Eran Yenko <dagnyeran@gmail.com>
2022-12-08 14:28:06 +0800

    Make card hover dynamic to screen

--- Deal With It/Assets/Scripts/Players/PlayerCardHover.cs
+++ Deal With It/Assets/Scripts/Players/PlayerCardHover.cs
@@ -5,32 +5,57 @@ using UnityEngine.EventSystems;
 
 public class PlayerCardHover : MonoBehaviour , IPointerEnterHandler, IPointerExitHandler
 {
-    Vector3 cardScale;
-    Vector2 cardPosition;
-    int cardIndexNumber;
+    Vector2 resolution;
+
+    Vector3 originalScale;
+    Vector2 originalPosition;
+    int originalIndex;
 
     public PlayerController playerController;
     private RoundController roundController;
 
     void Start(){
-        cardScale = this.transform.localScale;
-        cardPosition = this.transform.position;
-        cardIndexNumber = this.transform.GetSiblingIndex();
+        StartCoroutine(OnScreenChange());
+        originalIndex = this.transform.GetSiblingIndex();
 
         roundController = (RoundController)GameObject.FindGameObjectWithTag("Round Controller").GetComponent(typeof(RoundController));
+    }
+
+    // Resave positions when screen resolution changes
+    IEnumerator OnScreenChange()
+    {
+        SaveDimensions();
+
+        while (true)
+        {
+            if (resolution.x != Screen.width || resolution.y != Screen.height)
+            {
+                SaveDimensions();
+            }
+
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    // Save screen resolution, card scale, and card position
+    private void SaveDimensions()
+    {
+        resolution = new Vector2(Screen.width, Screen.height);
+        originalScale = this.transform.localScale;
+        originalPosition = this.transform.position;
     }
 
     public void OnPointerEnter(PointerEventData eventData){
         if ((playerController.ActionCardProject == true) && (roundController.PlayerTurn != -1)){
             this.transform.localScale = new Vector3(1.5F, 1.5F, 1.5F);
-            this.transform.position = new Vector2(this.transform.position.x, 350F);
+            this.transform.position = new Vector2(this.transform.position.x, Screen.height / 3);
             this.transform.SetSiblingIndex(5);
         }
     }
 
     public void OnPointerExit(PointerEventData eventData){
-        this.transform.localScale = cardScale;
-        this.transform.position = cardPosition;
-        this.transform.SetSiblingIndex(cardIndexNumber);
+        this.transform.localScale = originalScale;
+        this.transform.position = originalPosition;
+        this.transform.SetSiblingIndex(originalIndex);
     }
 }
